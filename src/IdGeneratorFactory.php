62665a2913818e8289f29e3006b2b7c5428f46b9
Rick Zimmermans (work) <rickzimmermans@gmail.com>
2025-03-25 10:51:57 +0100

    Add initial starting point option to IdGeneratorFactory

--- src/IdGeneratorFactory.php
+++ src/IdGeneratorFactory.php
@@ -27,6 +27,7 @@ class IdGeneratorFactory
             Arr::get($config, 'padding') ?? 5,
             Arr::get($config, 'prefix') ?? '',
             Arr::get($config, 'suffix') ?? '',
+            Arr::get($config, 'initial-starting-point') ?? 0
         );
     }
 
@@ -36,6 +37,7 @@ class IdGeneratorFactory
         int $paddingLength = 5,
         ?string $prefix = '',
         ?string $suffix = '',
+        int $initialStartingPoint = 0
     ): string {
         $prefix = $this->parseVariables($prefix);
         $suffix = $this->parseVariables($suffix);
@@ -78,6 +80,10 @@ class IdGeneratorFactory
 
         // stripped max id from maxId starting from prefix length and extracting according to adaptive length
         $strippedMaxId = Str::substr($maxId, $prefixLength, $adaptiveLength);
+
+        if (empty($strippedMaxId)) {
+            $strippedMaxId = $initialStartingPoint;
+        }
 
         $nextStrippedId = (int) $strippedMaxId + 1;
 
